2e2f58d15f53f9028c059e0ec451a29973d3515b
Leah <97891846+leahobot@users.noreply.github.com>
2024-06-03 03:13:05 +0100

    update image container (#3)

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,12 +11,16 @@ function Portfolio() {
 	const { activeTheme, largeHeader, smallHeader } = useStateContext();
 	const activeSection =  activeTheme === 'dark' ? 'bg-[rgba(9,14,22,0.95)] text-[#bdbdbd]' : 'bg-[#ffffff] text-[#3F4144]'
 
-	const imageBoxStyle =
-		"relative mb-[5rem] sm:mb-[7rem] group rounded-[7px] overflow-hidden shadow-[0_0_10px_0_rgba(0,0,0,0.2)] h-[15rem] sm:h-[20rem] lg:h-[30rem] transition-all duration-[1s]";
+	const imageBox =
+		"relative cursor-pointer w-[46%] group h-[15rem] sm:h-[20rem] lg:h-[30rem] mb-[5rem] sm:mb-[7rem] rounded-[8px] overflow-hidden shadow-[0_0_10px_0_rgba(0,0,0,0.2)] group-hover:scale-[1.05] group-hover:blur-[2px]";
 	const image =
-		"group-hover:scale-[1.05] group-hover:blur-[2px] object-cover h-full w-full transition-scale duration-[0.5s] object-cover";
+		"w-full h-full object-contain transition-scale duration-[0.5s] group-hover:scale-[1.05] group-hover:blur-[2px]";
 	const wrapperStyle =
 		"hidden absolute text-[1.4rem] sm:text-[1.8rem] text-white px-[2rem] sm:px-[3rem] font-semibold group-hover:flex items-center justify-center text-center inset-0 w-full h-full bg-black opacity-0 group-hover:opacity-[0.7] transition-opacity duration-[0.7s]";
+
+	const goToLink = (url) => {
+		window.open(url, '_blank', 'noreferrrer', 'noopenner');
+	}
 
 	return (
 		<section
@@ -32,13 +36,12 @@ function Portfolio() {
 					</p>
 					<p className="absolute top-[67%] lg:top-[65%] 2xl:top-[68%] w-[25%] sm:w-[15%] lg:w-[10%] h-[2px] sm:h-[3px] lg:h-[2px] xl:h-[3px] 2xl:h-[4px] bg-[var(--accent-color)] opa" />
 				</header>
+		
 				<main className="flex flex-wrap justify-between w-full mt-[5rem] sm:mt-[8rem]">
-					<a
-						href="https://www.klasha.com/"
-						alt="my-work"
-						target="_blank"
-						rel="noreferrer"
-						className={`${imageBoxStyle} w-[46%]`}>
+					<div
+						className={`${imageBox} w-[46%]`}
+						onClick={() => goToLink('https://www.klasha.com/')}
+					>
 						<img
 							src={klasha}
 							alt="my-work"
@@ -47,13 +50,10 @@ function Portfolio() {
 						<div className={wrapperStyle}>
 							<p>Klasha - Website</p>
 						</div>
-					</a>
-					<a
-						href="https://jays-store.vercel.app/"
-						alt="my-work"
-						target="_blank"
-						rel="noreferrer"
-						className={`${imageBoxStyle} w-[46%]`}>
+					</div>
+					<div
+						onClick={() => goToLink('https://jays-store.vercel.app/')}
+						className={imageBox}>
 						<img
 							src={jaysStore}
 							alt="my-work"
@@ -62,13 +62,10 @@ function Portfolio() {
 						<div className={wrapperStyle}>
 							<p>An E-commerce Store</p>
 						</div>
-					</a>
-					<a
-						href="https://app.torusmara.com/login"
-						alt="my-work"
-						target="_blank"
-						rel="noreferrer"
-						className={`${imageBoxStyle} w-[46%]`}>
+					</div>
+					<div
+						onClick={() => goToLink('https://app.torusmara.com/login')}
+						className={imageBox}>
 						<img
 							src={torus}
 							alt="my-work"
@@ -77,28 +74,22 @@ function Portfolio() {
 						<div className={wrapperStyle}>
 							<p>A Cross Border Payment Web Application</p>
 						</div>
-					</a>
-					<a
-						href="https://leahobot.github.io/jaysBank/"
-						alt="my-work"
-						target="_blank"
-						rel="noreferrer"
-						className={`${imageBoxStyle} w-[46%]`}>
+					</div>
+					<div
+						onClick={() => goToLink('https://leahobot.github.io/jaysBank/')}
+						className={imageBox}>
 						<img
 							src={jaysBank}
 							alt="my-work"
-							className="object-cover transition-all duration-[0.5s]"
+							className={image}
 						/>
 						<div className={wrapperStyle}>
 							<p>A Modern Bank Website</p>
 						</div>
-					</a>
-					<a
-						href="https://fitme.pages.dev/"
-						alt="my-work"
-						target="_blank"
-						rel="noreferrer"
-						className={`${imageBoxStyle} w-[100%]`}>
+					</div>
+					<div
+						onClick={() => goToLink('https://cleanax.vercel.app/')}
+						className={imageBox}>
 						<img
 							src={cleanax}
 							alt="my-work"
@@ -107,13 +98,10 @@ function Portfolio() {
 						<div className={wrapperStyle}>
 							<p>Cleanax - A Laundromat Web Application</p>
 						</div>
-					</a>
-					<a
-						href="https://fitme.pages.dev/"
-						alt="my-work"
-						target="_blank"
-						rel="noreferrer"
-						className={`${imageBoxStyle} w-[100%]`}>
+					</div>
+					<div
+						onClick={() => goToLink('https://fitme.pages.dev/')}
+						className={imageBox}>
 						<img
 							src={fitMe}
 							alt="my-work"
@@ -122,7 +110,7 @@ function Portfolio() {
 						<div className={wrapperStyle}>
 							<p>FitMe - Website</p>
 						</div>
-					</a>
+					</div>
 				</main>
 			</div>
 		</section>
@@ -130,3 +118,4 @@ function Portfolio() {
 }
 
 export default Portfolio;
+		
